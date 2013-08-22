f903236e6b69bb1b5e2cc0cb98a793d50b8764df
groyse <GuyRoyse@dswinc.com>
2013-08-22 11:35:15 -0400

    added size method

--- spec/stoopid_spec.js
+++ spec/stoopid_spec.js
@@ -1,26 +1,73 @@
-describe("Object", function() {
+describe('Object', function() {
 
   var subject;
 
   beforeEach(function() {
-    subject = {
-      foo: 'test',
-      bar: 42,
-      baz: true,
-      qux: {}
-    };
+
+    var parent = {};
+    parent.foo = 'test';
+
+    subject = Object.create(parent);
+    subject.bar = 42;
+    subject.baz = true;
+    subject.qux = {};
+
   });
 
-  describe("implements a forEach iterator", function() {
+  describe('size', function() {
+
+    it('returns the number of properties', function() {
+      expect(subject.size()).toBe(3);
+    });
+
+  });
+
+  describe('forEach', function() {
 
     it('calls the callback function for each item in the object', function() {
       var count = 0;
       subject.forEach(function() {
         count++;
       });
-      expect(count).toBe(4);
+      expect(count).toBe(3);
     });
 
+    it('passes the keys to the callback function', function() {
+      var keys = [];
+      subject.forEach(function(key) {
+        keys.push(key);
+      });
+      expect(keys).toEqual(['bar', 'baz', 'qux']);
+    });
+
+    it('passes the values to the callback function', function() {
+      var values = [];
+      subject.forEach(function(key, value) {
+        values.push(value);
+      });
+      expect(values).toEqual([42, true, {}]);
+    });
+
+    it('passes the object to the callback function', function() {
+      var callbackObject = {};
+      subject.forEach(function(key, value, object) {
+        callbackObject = object;
+      });
+      expect(callbackObject).toBe(subject);
+    });
+
+  });
+
+  describe('every', function() {
+  });
+
+  describe('some', function() {
+  });
+
+  describe('map', function() {
+  });
+
+  describe('reduce', function() {
   });
 
 });
--- src/stoopid.js
+++ src/stoopid.js
@@ -1,1 +1,10 @@
+Object.prototype.forEach = function(callback) {
+  var self = this;
+  Object.keys(self).forEach(function(key) {
+    callback(key, self[key], self);
+  });
+};
 
+Object.prototype.size = function() {
+  return Object.keys(this).length;
+};
